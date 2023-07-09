96be1fe04e8c4db7ad1c4034666826a6068f8205
Adam Iverson <aciverso@gmail.com>
2023-07-09 18:42:25 -0500

    init

---
+++ src/main/kotlin/com/example/initialtest/InitialtestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.initialtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InitialtestApplication
+
+fun main(args: Array<String>) {
+	runApplication<InitialtestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/initialtest/InitialtestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.initialtest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InitialtestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
